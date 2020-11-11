edb44e094739c6d2869a30a4be037d3d3f79c0dc
heyiwu <whuhyw@users.noreply.github.com>
2020-11-11 10:31:46 +0800

    refactor(user_info): handle errcode

--- user_info.go
+++ user_info.go
@@ -1,9 +1,16 @@
 package wework
 
+import (
+	"fmt"
+)
+
 func (c *WorkwxApp) GetUserID(code string) (string, error) {
 	rsp, err := c.execGetUserInfo(reqCode{Code: code})
 	if err != nil {
 		return "", nil
+	}
+	if !rsp.IsOK() {
+		return "", fmt.Errorf("get user info failed, errcode:%d, errormsg:%s", rsp.ErrCode, rsp.ErrMsg)
 	}
 	return rsp.UserId, nil
 }
